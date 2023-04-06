f28178d316174c5dc44c710cc3d6cf9a85d33f89
Kennedy <kiplangatkennedy019@gmail.com>
2023-04-06 10:13:15 +0300

    fone

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(myActivity="roller-skate"){
+
+    return `This Saturday, I want to ${myActivity}!`;
+
+}
+
+function mondayWork(activity="go to the office"){
+    return `This Monday, I will ${activity}.`;
+}
+// This convention below here worked. Let me try another one below here
+ function wrapAdjective(myAdjective="*"){
+    return function ( action="a hardworker"){
+       return `You are ${myAdjective}${action}${myAdjective}!`;
+
+    
+    }
+}
